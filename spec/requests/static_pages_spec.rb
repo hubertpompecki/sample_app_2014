35a6c869bfeb8dee2f6b1b47bc5c80360c13511d
Hubert Pompecki <hpompecki@gmail.com>
2014-06-28 22:03:20 +0100

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,10 +25,36 @@ describe "Static pages" do
         sign_in user
         visit root_path
       end
+
       it "should render the user's feed" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
+      end
+
+      describe 'with 2 microposts' do
+        it { should have_content('2 microposts') }
+      end
+
+      describe 'with 1 micropost' do
+        before do
+          user.microposts.destroy_all
+          FactoryGirl.create(:micropost, user: user)
+          visit root_path
+        end
+
+        it { should have_content('1 micropost') }
+      end
+
+      describe 'follower/following counts' do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link('0 following', href: following_user_path(user)) }
+        it { should have_link('1 followers', href: followers_user_path(user)) } 
       end
     end
   end
